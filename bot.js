bc694ad9a53e069e3a0e2ddf14a07df0c6779cb6
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2019-01-02 16:19:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const moment = require('moment');
 const client = new Discord.Client();
-const prefix = "T#"
+const prefix = "#"
 
 
 
@@ -297,17 +298,17 @@ client.on('message', message => {
          **
         [❖═════ Torento Commands ═══════❖]
         
-        『T#clear / لحذف الشات 』
-        『T#mc / لقفل الشات  』
-        『T#unmc / لفتح الشات 』
-        『T#bc / لارسال رسالة لجميع اعضاء السيرفر 』
-        『T#kick / لطرد شخص من الدسكورد 』
-        『T#ban / لاعطاء شخص باند من الدسكورد 』
-        『T#ct / لانشاء روم كتابي 』
-        『T#cv / لانشاء روم صوتي 』
-        『T#temp on / لتشغيل الرومات المؤقتة 』
-        『T#temp off / لاطفاء الرومات المؤقتة 』
-        『T#c-channel / لانشاء روم يكون بعدد اعضاء السيرفر 』
+        『#clear / لحذف الشات 』
+        『#mc / لقفل الشات  』
+        『#unmc / لفتح الشات 』
+        『#bc / لارسال رسالة لجميع اعضاء السيرفر 』
+        『#kick / لطرد شخص من الدسكورد 』
+        『#ban / لاعطاء شخص باند من الدسكورد 』
+        『#ct / لانشاء روم كتابي 』
+        『#cv / لانشاء روم صوتي 』
+        『#temp on / لتشغيل الرومات المؤقتة 』
+        『#temp off / لاطفاء الرومات المؤقتة 』
+        『#c-channel / لانشاء روم يكون بعدد اعضاء السيرفر 』
 
         
          **`);
@@ -317,7 +318,7 @@ client.on('message', message => {
         
         client.on ("guildMemberAdd", member => {
   
-          var role = member.guild.roles.find ("name", "Toronto");
+          var role = member.guild.roles.find ("name", "toronto-army");
           member.addRole (role);
          
        })
@@ -325,5 +326,73 @@ client.on('message', message => {
        client.on ("guildMemberRemove", member => {
           
        })
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + `bot-ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+            .setColor('#000000').setColor('#36393e')
+        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **-, NetFlixSupport** will be here soon to help.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '=confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
+
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 14) {
+    member.ban("وهمي ( اقل من اسبوعين ) انشاء الحساب .")
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
